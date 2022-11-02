a05ee591402a09bcee16efff26da8fce152c89b5
LethalWarrior <albertfiraz@gmail.com>
2022-11-02 19:57:07 +0700

    Change Comic data class to be available as Parcelable

--- app/src/main/java/com/dicoding/comicstore/Comic.kt
+++ app/src/main/java/com/dicoding/comicstore/Comic.kt
@@ -1,15 +1,58 @@
 package com.dicoding.comicstore
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Comic(
-    var title: String = "",
-    var description: String = "",
-    var genre: String = "",
-    var author: String = "",
-    var publisher: String = "",
-    var isbn: String = "",
+    var title: String? = "",
+    var description: String? = "",
+    var genre: String? = "",
+    var author: String? = "",
+    var publisher: String? = "",
+    var isbn: String? = "",
     var pages: Int = 0,
     var cover: Int = 0,
-    var publicationDate: String = "",
-    var price: String = ""
+    var publicationDate: String? = "",
+    var price: String? = ""
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readInt(),
+        parcel.readInt(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
 
-)
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(description)
+        parcel.writeString(genre)
+        parcel.writeString(author)
+        parcel.writeString(publisher)
+        parcel.writeString(isbn)
+        parcel.writeInt(pages)
+        parcel.writeInt(cover)
+        parcel.writeString(publicationDate)
+        parcel.writeString(price)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Comic> {
+        override fun createFromParcel(parcel: Parcel): Comic {
+            return Comic(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Comic?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
